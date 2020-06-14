f07fcf6c92fdb7e6f85308715085c1af2c9bda59
Matt Eads <matteads@Matts-MacBook-Pro.local>
2020-06-13 22:18:17 -0400

    updated placeholder test

---
+++ tests/test_search.py
@@ -0,0 +1,14 @@
+def test_basic_search():
+    pass
+
+def test_filetype_search():
+    pass
+
+def test_site_search():
+    pass
+
+def test_combo_search():
+    pass
+
+def test_no_search():
+    pass
